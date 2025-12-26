386f8b4d4d0e9d9d512f34bfaf2ceb889103d6d8
Vihang Jadhav <vihang.jadhav155@gmail.com>
2025-12-26 18:43:50 +0530

    add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import gsap from "gsap"
 import { ScrollTrigger, SplitText } from "gsap/all"
-gsap.registerPlugin(ScrollTrigger, SplitText) 
+gsap.registerPlugin(ScrollTrigger, SplitText)
+import Navbar from "./components/navbar"
+import Hero from "./components/Hero"
 
 const App = () => {
   return (
-    <div className="flex-center h-[100vh]">
-      <h1 className="text-3xl font-bold underline text-amber-300">
-        Hello world!
-      </h1>
-    </div>
+   <main>
+    <Navbar />
+    <Hero/>
+   </main>
   )
 }
 
