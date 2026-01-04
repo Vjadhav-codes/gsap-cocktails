0d6d45f08750958e2b41d6a371f548812702d65f
Vihang Jadhav <vihang.jadhav155@gmail.com>
2026-01-04 15:16:47 +0530

    Implement about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 gsap.registerPlugin(ScrollTrigger, SplitText);
@@ -6,14 +5,6 @@ import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
-=======
-import gsap from "gsap"
-import { ScrollTrigger, SplitText } from "gsap/all"
-gsap.registerPlugin(ScrollTrigger, SplitText)
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import Cocktails from "./components/Cocktails"
->>>>>>> a06ce23f840b8e66215a52af18512f0fe0e691d5
 
 const App = () => {
   return (
@@ -25,9 +16,4 @@ const App = () => {
     </main>
   );
 };
-
-<<<<<<< HEAD
 export default App;
-=======
-export default App
->>>>>>> a06ce23f840b8e66215a52af18512f0fe0e691d5
