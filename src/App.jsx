1175f5ccd12fccf1657d69fb6e2bcd0f99e01bbe
Vihang Jadhav <vihang.jadhav155@gmail.com>
2026-01-04 16:09:19 +0530

    Implement the art section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
+import Art from './components/Art'
 
 const App = () => {
   return (
@@ -12,7 +13,8 @@ const App = () => {
       <Navbar />
       <Hero />
       <Cocktails />
-      <About/>
+      <About />
+      <Art />
     </main>
   );
 };
