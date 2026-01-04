a06ce23f840b8e66215a52af18512f0fe0e691d5
Vihang Jadhav <Vihang.jadhav155@gmail.com>
2026-01-04 12:23:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import gsap from "gsap"
 import { ScrollTrigger, SplitText } from "gsap/all"
 gsap.registerPlugin(ScrollTrigger, SplitText)
-import Navbar from "./components/navbar"
+import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Cocktails from "./components/Cocktails"
 
